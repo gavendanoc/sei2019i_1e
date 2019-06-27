64729a60b4574f4f52e788093121c1c327d3f74a
jdacostabe <48451314+jdacostabe@users.noreply.github.com>
2019-06-27 10:48:36 -0500

    Add files via upload

---
+++ code/Photo editor/app/src/main/java/com/example/filesystemtest/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.filesystemtest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void Login(View view) {
+        Intent home = new Intent(this, HomeActivity.class);
+        startActivity(home);
+    }
+}
