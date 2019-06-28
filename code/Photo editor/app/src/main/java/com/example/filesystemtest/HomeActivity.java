11f6a6b00a7afa6cde0c9fa8c40ab429414ec9c8
julian acosta <jdacostabe@unal.edu.co>
2019-06-27 19:35:34 -0500

    Base code update

--- code/Photo editor/app/src/main/java/com/example/filesystemtest/HomeActivity.java
+++ code/Photo editor/app/src/main/java/com/example/filesystemtest/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.example.filesystemtest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,5 +15,10 @@ public class HomeActivity extends AppCompatActivity {
 
     public void Camera(View view){
 
+    }
+
+    public void Back(View view){
+        Intent back= new Intent(this, MainActivity.class);
+        startActivity(back);
     }
 }
