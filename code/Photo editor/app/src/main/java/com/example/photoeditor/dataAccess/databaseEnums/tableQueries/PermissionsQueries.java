29321883931ccc0d07424a060d3e709da62dcba5
Gabriel <gabriel19oct@hotmail.com>
2019-07-10 21:06:05 -0500

    added repositories, models, request, queries for permissions

--- code/Photo editor/app/src/main/java/com/example/photoeditor/dataAccess/databaseEnums/tableQueries/PermissionsQueries.java
+++ code/Photo editor/app/src/main/java/com/example/photoeditor/dataAccess/databaseEnums/tableQueries/PermissionsQueries.java
@@ -1,4 +1,27 @@
 package com.example.photoeditor.dataAccess.databaseEnums.tableQueries;
 
+import com.example.photoeditor.dataAccess.databaseEnums.TableNames;
+import com.example.photoeditor.dataAccess.databaseEnums.tableFields.UserFields;
+
 public enum PermissionsQueries {
+
+    SelectAllPermissions(
+            "SelectAllPermissions",
+            "SELECT* FROM " + TableNames.Permissions.getKey())
+    ;
+
+    private String type;
+    private String query;
+
+    private PermissionsQueries(String type, String query){
+        this.type = type;
+        this.query=query;
+    }
+
+    public String getFormat(Object []args ){
+        return String.format(query , args);
+    }
+    public String getType(){
+        return this.type;
+    }
 }
--- code/Photo editor/app/src/main/java/com/example/photoeditor/dataAccess/requests/PermissionsRequest.java
+++ code/Photo editor/app/src/main/java/com/example/photoeditor/dataAccess/requests/PermissionsRequest.java
@@ -1,4 +1,24 @@
 package com.example.photoeditor.dataAccess.requests;
 
-public class PermissionsRequest {
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.Map;
+
+import static com.android.volley.Request.*;
+
+public class PermissionsRequest extends StringRequest {
+    private static final String route = "https://aramonhred.000webhostapp.com/fotos/PermissionsManager.php";
+
+    private Map<String, String > parameters;
+
+    public PermissionsRequest(Map<String, String > parameters, Response.Listener<String> listener){
+        super(Method.POST, route, listener, null);
+        this.parameters = parameters;
+    }
+
+    protected Map<String,String> getParams(){
+        return parameters;
+    }
 }
